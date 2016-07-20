e3aafcfb9f55114832edb9630b094310f1d62e20
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2016-07-19 21:05:55 -0300

    Done - 8. Adding user accounts

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
@@ -39,8 +40,11 @@ Template.body.events({
 		Tasks.insert({
 			text,
 			createadAt: new Date(),
+			owner: Meteor.userId(),
+			username: Meteor.user().username,
 		});
 
+		// clear form after task added
 		target.text.value = '';
 
 	},
